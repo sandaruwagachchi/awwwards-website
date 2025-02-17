ce6a7b147b0d9d85226f05a3b88874fe7a0884f9
Sandaru Wagachchi <151208394+sandaruwagachchi@users.noreply.github.com>
2025-02-17 23:19:51 +0530

    Create hero secttion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './Components/Hero'
 
 const App = () => {
   return (
-    <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>Welcome to Awwwards</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+     <Hero/>
     </main>
   )
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useRef } from 'react'
+import Button from './Button';
+
+const Hero = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const [hasClicked, setHasClicked] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedVideos, setLoadedVideos] = useState(0);
+
+    const totalVideos = 3;
+    const nextVideoRef = useRef(null);
+
+    const handleVideoLoad = () =>{
+        setLoadedVideos((prev) => prev + 1)
+    }
+
+    // 0 % 4 = 0 + 1 => 1
+    // 1 % 4 = 1 + 1 => 2
+    // 2 % 4 = 2 + 1 => 3
+    // 3 % 4 = 3 + 1 => 4
+    // 4 % 4 = 0 + 1 => 1
+    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+    const handleMiniVdClick = () => {
+        setHasClicked(true);
+        setCurrentIndex(upcomingVideoIndex);
+    }
+
+    const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+      <div id='video-frame' className='relative z-10 
+      h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
+        <div>
+            <div className='mask-clip-path absolute-center
+             absolute z-50 size-64 cursor-pointer overflow-hidden
+             rounded-lg'>
+            <div onClick={handleMiniVdClick} className='origin-center
+             scale-50 opacity-0 transition-all duration-500 ease-in
+             hover:opacity-100'>
+                <video 
+                ref={nextVideoRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id='current-video'
+                className='size-64 origin-center scale-150 object-cover
+                object-center'
+                onLoadedData={handleVideoLoad}
+                />
+            </div>
+            </div>
+            <video
+               ref={nextVideoRef}
+               src={getVideoSrc(currentIndex)}
+               loop
+               muted
+               id='next-video'
+               className='absolute-center invisible absolute z-20
+               size-64 object-cover object-center'
+               onLoadedData={handleVideoLoad}
+               />
+               <video 
+               src = {getVideoSrc(currentIndex === totalVideos - 1 
+                ? 1 : currentIndex)}
+                autoPlay
+                loop
+                muted
+                className='absolute left-0 top-0 size-full object-cover object-center'
+                onLoadedData={handleVideoLoad}
+                />
+            </div>
+            <h1 className='special-font hero-heading absolute bottom-5 right-5
+             z-40 text-blue-75'>
+              G<b>a</b>mi<b>n</b>g
+             </h1>
+             <div className='absolute left-0 top-0 z-40 size-full'>
+              <div className='mt-24 px-5 sm:px-10'>
+                <h1 className='special-font hero-heading text-blue-100'>
+                  re<b>d</b>efi<b>n</b>e
+                </h1>
+                <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
+                  Enter the Metagame Layer <br/> Unleash the Play Economy
+                </p>
+                <Button />
+              </div>
+             </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
