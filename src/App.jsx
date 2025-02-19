d17210d3d6aec10dbcbc83c85dbc16e35efb8166
Sandaru Wagachchi <151208394+sandaruwagachchi@users.noreply.github.com>
2025-02-19 20:13:58 +0530

    Completed hero section and about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Components/Hero'
+import About from './Components/About'
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
      <Hero/>
+     <About/>
     </main>
   )
 }
