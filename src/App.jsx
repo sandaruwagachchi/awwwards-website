1cfca6248f4ebfdc9e19a945aac1c4dd23ea2502
Sandaru Wagachchi <151208394+sandaruwagachchi@users.noreply.github.com>
2025-02-20 19:25:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Hero from './Components/Hero'
 import About from './Components/About'
 import Navbar from './Components/Navbar'
+import Features from './Components/Features'
+import Story from './Components/Story'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
@@ -9,6 +13,10 @@ const App = () => {
      <Navbar/>
      <Hero/>
      <About/>
+     <Features/>
+     <Story/>
+     <Contact/>
+     <Footer/>
     </main>
   )
 }
